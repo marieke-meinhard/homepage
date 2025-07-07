fe2305b5389eeb4e23ab17bd4579fc81b6ec9a51
Marieke Meinhard <mariekemeinhard@MacBookAir.fritz.box>
2025-07-07 22:53:07 +0200

    upload homepage

---
+++ resources/js/unite.gallery.js
@@ -0,0 +1,56 @@
+jQuery(document).ready(function () {
+    
+    jQuery("#researchdata").unitegallery({
+        tile_border_color: "#7a7a7a",
+        tile_outline_color: "#8B8B8B",
+        tile_enable_shadow: false,
+        tile_shadow_color: "#8B8B8B",
+        tile_overlay_opacity: 0.5,
+        tile_show_link_icon: true,
+        tile_image_effect_type: "sepia",
+        tile_image_effect_reverse: true,
+        tile_enable_textpanel: true,
+        tile_link_newpage: true,
+        tile_textpanel_always_on: true,
+        tile_textpanel_bg_opacity: 1,
+        tile_textpanel_bg_color: "#464545",
+        lightbox_textpanel_title_color: "e5e5e5",
+        tiles_type: "justified",
+        tiles_col_width: 230,
+        tiles_space_between_cols: 5,
+        tiles_justified_row_height: 170
+    });
+    jQuery("#gallery").unitegallery({
+        gallery_theme: "grid",
+        grid_num_cols: 1,
+        gallery_autoplay: true, //true / false - begin slideshow autoplay on start
+        gallery_play_interval: 5000, //play interval of the slideshow
+        gallery_pause_on_mouseover: true, //true,false - pause on mouseover when playing slideshow true/false
+        theme_panel_position: "right",
+        gallery_background_color: "gray",
+        slider_textpanel_bg_color: "rgba(0, 0, 0, 0.5)", //textpanel background color
+        gridpanel_background_color: "rgba(0, 0, 0, 0.5)",
+        slider_scale_mode: "down", //down: scale down only, smaller images will be shown, don't enlarge images (scale up)
+        thumb_height: 88
+        /*thumb_fixed_size:true //true,false - fixed/dynamic thumbnail width*/
+    });
+    jQuery("#gallerie").unitegallery({
+    gallery_theme: "tilesgrid",
+    gallery_width:"100%",	//gallery width
+    gallery_min_width: 150,	//gallery minimal width when resizing
+    gallery_background_color: "tomato",	//set custom background color. If not set it will be taken /from css.
+    theme_gallery_padding: 0,	//padding from sides of the gallery
+    grid_padding:20,		//set padding to the grid
+       tile_width: 120,		//tile width
+       tile_height: 120,		//tile height
+       grid_num_rows:10,	//maximum number of grid rows. If set to big value, the navigation will not appear.
+       tile_show_link_icon:true,
+        tile_enable_textpanel: true,
+        tile_textpanel_title_text_align: "center",
+        theme_navigation_type: "bullets",
+        theme_bullets_margin_top: 20, //the space to bullets
+        theme_bullets_margin_bottom: 20, //the space to bullets
+        theme_bullets_color: "gray", //gray, blue, brown, green, red - color of the bullets
+        bullets_space_between: 12 //space between bullets
+    });
+});
